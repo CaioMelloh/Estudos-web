f136006873df9be01ccc541e1311f895f9146a04
CaioMelloh <caiohaackmello@gmail.com>
2022-10-12 12:53:42 -0300

    Estudos NOdeJS

---
+++ EstudosNodeJS/testeIndependent/functionAge.js
@@ -0,0 +1,6 @@
+let idade = function(birthday, actuallyYear){
+    let age = actuallyYear - birthday;
+    return age + " anos de idade.";
+}
+
+module.exports = idade;
--- EstudosNodeJS/testeIndependent/index.js
+++ EstudosNodeJS/testeIndependent/index.js
@@ -3,3 +3,9 @@ console.log(max(7, 5, 5, 8));
 
 let min = require("./functionMin");
 console.log(min(0, 5, 8, 0))
+
+//------------------------------
+
+
+let funcao = require("./functionAge");
+console.log(funcao(2002, 2022));

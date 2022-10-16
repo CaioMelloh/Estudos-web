e81a5f6bdb01872377b4301c691a241b8c5250ed
CaioMelloh <caiohaackmello@gmail.com>
2022-10-15 23:37:58 -0300

    Estudos e treinos

--- JS/TreinoSlice/index.js
+++ JS/TreinoSlice/index.js
@@ -1,6 +1,10 @@
 /*o Slice extrai uma parte do string e devolve, sendo que a string original permanece intacta
 o primeiro número pode estar sozinho, sendo que ao usar negativo, será feito o length - numero escolhido do que será
-retirado*/
+retirado
+*/
+
 let nome = "Jorginho";
-let novo = nome.slice(-3);
+let novo = nome.slice(-3); /* (nome.length = 8 )-3) (8 -3) => (5) A partir do número 5 até o final*/
 console.log(novo);
+
+

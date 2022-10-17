dfb273d33d1f117c3a08614b2d4b9eff794be200
CaioMelloh <caiohaackmello@gmail.com>
2022-10-17 15:15:03 -0300

    Exercicios from exercism

--- JS/questsFromExercism/quest5/index.js
+++ JS/questsFromExercism/quest5/index.js
@@ -2,6 +2,13 @@
 Implementar a função frontDoorResponse isso pega uma linha do poema como argumento
  e retorna a primeira letra dessa linha.*/
 
+
+function capitalize(word){
+  
+  return (word.slice(0, 1)).toUpperCase() + word.slice(1, word.length).toLowerCase();
+  
+}
+
  function frontDoorResponse(line) {
     return line[0];
   }
@@ -12,32 +19,18 @@ Implementar a função frontDoorPassword que aceita uma sequência
 ( das letras combinadas que você encontrou na tarefa 1 ) 
 e a retorna capitalizada corretamente.
 */
-/*function backDoorPassword*/
+/*function frontDoorPassword*/
 
-function backDoorPassword(nome){
-    /* primeira letra maiúscula e resto em minúsculo sempre*/
-    let novo = nome.toLowerCase();
-    let primeira = (novo.slice(0, 1)).toUpperCase();
-
-    /*let result = novo.split(1, 2);*/
-    novo = novo.slice(1, novo.length);
-    let result = primeira + novo;
-    return result;
+function frontDoorPassword(word) {
+  return capitalize(word);
 }
-
 
 
 /*3.
 Implementar a função backDoorResponse que pega uma linha do poema como argumento e retorna a última 
 letra dessa linha que não é um personagem do espaço em branco*/
 function backDoorResponse(line) {
-
-    let newWord = line.trim();
-
-    let formatLetter = newWord.slice((newWord.length - 1), newWord.length);
-
-    return formatLetter;
-
+  return line.trimEnd().slice(-1)
 }
 
 /*4.Para entrar no clube de poesia pela porta dos fundos, você precisa ser educado. 
@@ -45,7 +38,8 @@ Portanto, para derivar a senha, desta vez você precisa capitalizar corretamente
 ', please' no final. Implementar a função backDoorPassword que aceita uma sequência 
 ( das letras combinadas encontradas na tarefa 3 ) e retorna a versão educada da senha em maiúscula.*/
 function backDoorPassword(word) {
-    word = frontDoorPassword(word);
-    let gentleman = word + ", please";
-    return gentleman;
-  }
+  
+  return capitalize(word) + ", please";;
+}
+
+

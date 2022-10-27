a60bef72e1528fff8d1f4fc8b4572a086d58101b
CaioMelloh <caiohaackmello@gmail.com>
2022-10-26 21:01:44 -0300

    Conclui desafio Arrays

--- DesafiosOneBitCode/DesafiosJS/desafioArray/index.js
+++ DesafiosOneBitCode/DesafiosJS/desafioArray/index.js
@@ -15,13 +15,13 @@ function filter(array){
     for(let i= 0; i<array.length; i++){
         if(array[i][1] > 9)
             {
-                novoarray.push(array[i][0]);
+                novoarray.push(" "+array[i][0]+"");
             }
 }
-    console.log(novoarray);
+    return novoarray;
 }
 
-/*filter(hitchedSpaceships);*/
+let filtrado = filter(hitchedSpaceships);
 
 /*-----------------------------------------------------------*/
 
@@ -46,10 +46,40 @@ function buscador(array){
 
     function plataforma(array){
 
-        console.log(array.findIndex(buscador) + 1);
+        return array.findIndex(buscador) + 1;
 
     }
 
-    plataforma(hitchedSpaceships);
+    let plataform_numeration = plataforma(hitchedSpaceships);
 /* o buscador irá percorrer todos os elementos do hitchedSpaceship*/
 
+/*-------------------------------------------------------------------------*/
+/*Mostre o nome de cada nave em caixa alta*/
+
+/*função que percorreria apenas a primeira posição das camadas do meu array
+    usaria o método to uppercase e imprimiria*/
+
+
+
+    function gritandoNomeDasNaves(array){
+        let arrayUpper = new Array();
+        for(let i = 0; i<array.length; i++){
+
+            for(let j = 0 ; j<array[i].length; j++){
+
+                if(typeof(array[i][j]) == "string" )
+                {
+                    arrayUpper.push(" "+array[i][j].toUpperCase()+" ");
+                    continue;
+                    
+                }
+            }
+        }
+        return arrayUpper;
+    }
+  
+
+
+    let nomesEmCaixaAlta = gritandoNomeDasNaves(hitchedSpaceships);
+
+    alert(`As naves com mais de 9 tripulantes são ${filtrado}\nO número da plataforma com engate pendente é ${plataform_numeration}\nOs nomes das naves possíveis são: ${nomesEmCaixaAlta}`);

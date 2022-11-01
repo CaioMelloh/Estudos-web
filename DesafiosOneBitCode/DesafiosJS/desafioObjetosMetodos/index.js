ab10c40eed7fb5c47166a7af93b31a35ad76de59
CaioMelloh <caiohaackmello@gmail.com>
2022-11-01 16:08:37 -0300

    Estuda seletores id class

--- DesafiosOneBitCode/DesafiosJS/desafioObjetosMetodos/index.js
+++ DesafiosOneBitCode/DesafiosJS/desafioObjetosMetodos/index.js
@@ -1,5 +1,13 @@
 
-/* tipos string nas respostas, converter para number*/
+
+/*
+!!!!!!!!!!!!!!!!!!
+Possível melhoria, como colocar um atributo que seja um objeto painel de controle
+seria uma boa talvez
+!!!!!!!!!!!!!!!!!!!
+*/
+
+/*objeto nave*/
 
 let nave = {
     velocidade: 0,
@@ -9,48 +17,59 @@ let nave = {
     acelerar: function(){
         let quantidade = prompt("O quanto você gostaria de acelerar? Velocidade atual : "+this.velocidade);
         quantidade = Number(quantidade);
-        verificarVelocidade(this.velocidade, this.velocidadeMax, quantidade);
+        controlPanel.verify(this.velocidade, this.velocidadeMax, quantidade);
       
         
     },
+
     parar: function(){
-        alert("O nome da nave é "+this.nome+"\nO tipo da nave é: "+this.tipo+"\nSua velocidade atual era de "+this.velocidade+"m/s");
+        alert("O nome da nave é "+nave.nome+"\nO tipo da nave é: "+nave.tipo+"\nSua velocidade atual era de "+nave.velocidade+"m/s");
+    }
+}
+
+/*objeto painel de controle*/
+
+let controlPanel = {
+    verify:
+    function verificarVelocidade(velocidade, velocidadeMax, quantidade){
+        if(velocidade + quantidade >= velocidadeMax ){
+            alert("Velocidade máxima atingida!");
+            nave.velocidade = 60;                 /*testar o loop de aceleração e retardo da próxima vez*/
+        }
+        else{
+            nave.velocidade+= quantidade;
+        }
     },
-    
     action: function(){
         let question;
         do{
             question = prompt("Você gostaria de acelerar ou parar? A / P");
             if(question == 'A' || question == 'a'){
-                this.acelerar();
+                nave.acelerar();
             }
             else if(question == 'P' || question == 'p'){
-                this.parar();
+                nave.parar();
             }
             else{console.log("Opção inválida!");}
         }while(question != 'P');
 
-    },
-    cadastro: function(){
-        this.nome = prompt("Digite o nome da nave: ");
-        this.tipo = prompt("Digite o tipo da nave: ");
-        this.velocidadeMax = prompt("Digite qual será a velocidade máxima permitida: ");
-        this.action();
-        /*ação*/
     }
-
 
 }
 
-/*OK*/
-function verificarVelocidade(velocidade, velocidadeMax, quantidade){
-    if(velocidade + quantidade >= velocidadeMax ){
-        alert("Velocidade máxima atingida!");
-        nave.velocidade = 60;                 /*testar o loop de aceleração e retardo da próxima vez*/
-    }
-    else{
-        nave.velocidade+= quantidade;
+
+/*objeto cadastro*/
+
+let cadastro = 
+{
+    cadastrar: function(){
+        nave.nome = prompt("Digite o nome da nave: ");
+        nave.tipo = prompt("Digite o tipo da nave: ");
+        nave.velocidadeMax = prompt("Digite qual será a velocidade máxima permitida: ");
+        controlPanel.action();
+        
     }
 }
 
-nave.cadastro();
+cadastro.cadastrar();
+

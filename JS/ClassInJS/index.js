39de46f7c71db7c79bea6c1f0d41a18c365c10ff
CaioMelloh <caiohaackmello@gmail.com>
2022-11-01 15:32:09 -0300

    Estudo Objetos e Classes

---
+++ JS/ClassInJS/index.js
@@ -0,0 +1,29 @@
+/*
+let objeto = {};
+let objeto2 = new Object();
+console.log(objeto2);
+*/
+/*inicialização de objetos*/
+
+/*
+class Carro {
+
+};
+
+let carrin = new Carro();
+/*inicialização da classe carro */
+
+
+
+class Pirata{
+    constructor(nome, recompensa){
+        this.nome = nome;
+        this.recompensa = recompensa;
+    }
+}
+
+let mugiwaraNakama1 = new Pirata("Chooper", 50);
+
+console.log(mugiwaraNakama1);
+
+

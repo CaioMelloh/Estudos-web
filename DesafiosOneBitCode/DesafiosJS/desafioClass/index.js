1d1ce72601497e049ac6c8d499b7cf222c987501
CaioMelloh <caiohaackmello@gmail.com>
2022-11-03 14:06:00 -0300

    Exercicios js

--- DesafiosOneBitCode/DesafiosJS/desafioClass/index.js
+++ DesafiosOneBitCode/DesafiosJS/desafioClass/index.js
@@ -1,25 +1,35 @@
 class spaceship {
     engate(){
-        let nome = prompt("Digite um nome para nave: ");
-        let tripulantes = Number(prompt("Qual a quantidade de tripulantes?"));
+        let nome = prompt("Digite seu nome: ");
+        let tripulantes = Number(prompt("Digite a quantidade de tripulantes: "));
+
         this.nome = nome;
         this.tripulantes = tripulantes;
+
+        this.portas = true;
+        this.engatePendente = false;
     }
 }
 
-function menu(){
-    let resp = prompt("Em que posso ajudá-lo?"
-    +"\n1-Realizar engate\n2-Imprimir Naves"
-    +"\n3-Sair do programa");
+let naves = [];
+
+
+let resp;
+
+do{
+    resp = prompt("Digite a nave que deseja");
     switch(resp){
         case "1":
             let nave = new spaceship();
             nave.engate();
-            estacaoEspacial.push(nave);
+            naves.push(nave);
             break;
+        case "2":
+            alert(JSON.stringify(naves));
+            break;
+        case "3":
+            console.log("Sistema encerrado!");
+            break ;
+            
     }
-}
-
-let estacaoEspacial = [];
-
-menu();
+}while(resp != 3);
